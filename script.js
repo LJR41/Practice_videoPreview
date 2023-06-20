3f31aa478c815944535e37a1e113d320dadc50ac
LJR411~ <ljremulla@yahoo.com>
2023-06-19 17:44:19 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function playVideo(){
+    var video = document.querySelector("#waterfall")
+    video.play()
+    
+}
+
+function pauseVideo(){
+    var video = document.querySelector("#waterfall")
+    video.pause()
+    
+}
